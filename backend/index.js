18dea62ce57ec0f477d7613a2552ff1a64dc09db
@sonam-nayak <nayaksonam955677@gmail.com>
2025-09-14 20:42:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ let app = express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["https://onecart-frontend-kz7r.onrender.com", "http://localhost:5174"],
   credentials: true
 }))
 app.use("/api/auth", authRoutes)
